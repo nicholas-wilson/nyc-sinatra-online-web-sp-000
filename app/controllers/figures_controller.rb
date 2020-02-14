eec9ee5e1c0ad163917cbb00b246648d88790530
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-14 21:12:54 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,66 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    # binding.pry
+    @figures = Figure.all
+    erb :"/figures/index"
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"/figures/new"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/edit"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/show"
+  end
+
+  post '/figures' do
+    figure = Figure.new                         # Make the new figure & name it
+    figure.name = params[:figure][:name]
+    figure.save
+    if params[:title][:name] != ''              #Check to see if a new title was given
+      if !params[:figure][:title_ids]
+        params[:figure][:title_ids] = []
+      end
+      title = Title.new
+      title.name = params[:title][:name]
+      title.save
+      params[:figure][:title_ids] << title.id
+    end
+    figure.title_ids = params[:figure][:title_ids]
+    if params[:landmark][:name] != ''              #Check to see if a new landmark was given
+      landmark = Landmark.new
+      landmark.name = params[:landmark][:name]
+      if params[:landmark][:year_completed] != ''
+        landmark.year_completed = params[:landmark][:year_completed]
+      end
+      landmark.save
+      if !params[:figure][:landmark_ids]
+        params[:figure][:landmark_ids] = []
+      end
+      params[:figure][:landmark_ids] << landmark.id
+    end
+      figure.landmark_ids = params[:figure][:landmark_ids]
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find_by(params[:id])
+    figure.update(params[:figure])
+    redirect :"/figures/#{params[:id]}"
+  end
+
+  delete '/figures/:id' do
+
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,29 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do
+
+  end
+
+  get '/landmarks/new' do
+
+  end
+
+  get '/landmarks/:id/edit' do
+
+  end
+
+  get '/landmarks/:id' do
+
+  end
+
+  post '/landmarks' do
+
+  end
+
+  patch '/landmarks/:id' do
+
+  end
+
+  delete '/landmarks/:id' do
+
+  end
 end
